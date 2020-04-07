58eac0289ebd45a4420d58e882dc2843e0772b18
prive2 <prive2@monsanto.com>
2020-04-06 22:27:03 -0300

    init with express and add gitignore and eslint

--- src/modules/mylog.js
+++ src/modules/mylog.js
@@ -1,9 +1,9 @@
-module.exports.info = function info (text){
-    console.log("info", text)
-    return text;
-}
+module.exports.info = function info(text) {
+  console.log('info', text);
+  return text;
+};
 
-module.exports.error = function error (text){
-    console.log("error", text)
-    return text;
-}
+module.exports.error = function error(text) {
+  console.log('error', text);
+  return text;
+};
